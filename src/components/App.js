dba562b141782a3a5a62a9e279d452cf48fbaea6
dreamer <wildetangeant@gmail.com>
2019-05-30 15:07:24 -0700

    works

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,8 @@ import AllCharacters from '../containers/characters/AllCharacters';
 
 export default function App() {
   return (
-    <>
-    {/* <h1>fuckthis</h1> */}
+    
     <AllCharacters />
-    </>
+    
   );
 }
--- src/services/avatarApi.js
+++ src/services/avatarApi.js
@@ -1,16 +1,20 @@
 import defaultImg from '../assetts/bagua-512.png';
 
-export const getCharacters = () => {
-  return fetch('https://last-airbender-api.herokuapp.com/api/v1/characters')
+const get = url => {
+  return fetch(url)
     .then(res => ([res.ok, res.json()]))
     .then(([ok, json]) => {
       if(!ok) throw 'unable to fetch';
+
       return json;
-    })
+    });
+};
+
+export const getCharacters = () => {
+  return get('https://last-airbender-api.herokuapp.com/api/v1/characters')
     .then(json => json.map(character => ({
       id: character._id,
       name: character.name,
       img: character.photoUrl || defaultImg
     })));
-}
-;
+};
