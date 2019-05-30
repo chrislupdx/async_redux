530412f59743b071048c9b5de96b9f4a627ef99b
dreamer <wildetangeant@gmail.com>
2019-05-30 16:18:19 -0700

    hello world on detail view

--- src/containers/characters/CharacterById.js
+++ src/containers/characters/CharacterById.js
@@ -1,45 +1,48 @@
 import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
+// import { connect } from 'react-redux';
 
 class CharacterById extends PureComponent {
-    static propTypes = {
-      character: PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        image: PropTypes.string.isRequired,
-        position: PropTypes.string.isRequired
-      }),
-      loading: PropTypes.bool.isRequired,
-      fetch: PropTypes.func.isRequired
-    }
+  // static propTypes = {
+  //   character: PropTypes.shape({
+  //     id: PropTypes.string.isRequired,
+  //     name: PropTypes.string.isRequired,
+  //     image: PropTypes.string.isRequired,
+  //     position: PropTypes.string.isRequired
+  //   }),
+  //   loading: PropTypes.bool.isRequired,
+  //   fetch: PropTypes.func.isRequired
+  // }
 
-    componentDidMount() {
-      this.props.fetch();
-    }
+  // componentDidMount() {
+  //   this.props.fetch();
+  // }
+  render() {
+    console.log('hi');
+    // const { character, loading } = this.props;
 
-    render() {
-      const { character, loading } = this.props;
+    // if(loading) return <h1> Loading </h1>;
 
-      if(loading) return <h1> Loading </h1>;
-
-      return <p>whatever</p>;
-    }
+    return <p>nooo</p>;
+  }
 }
 
-const mapStateToProps = state => ({
-  character: getCharacter(state),
-  loading: getCharacterLoading(state)
-});
+// const mapStateToProps = state => ({
+//   character: getCharacter(state),
+//   loading: getCharacterLoading(state)
+// });
 
-const mapDispatchToProps = (dispatch, props) => ({
-  fetch() {
-    dispatch(fetchCharacter(props.match.params.id));
-  }
-});
+// const mapDispatchToProps = (dispatch, props) => ({
+//   fetch() {
+//     dispatch(fetchCharacter(props.match.params.id));
+//   }
+// });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(CharacterById)
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(CharacterById)
+// ;
+
+export default CharacterById
 ;
