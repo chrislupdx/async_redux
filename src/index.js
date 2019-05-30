64e985e633ed4095f3fcb5cc7fba1648df94f2f2
dreamer <wildetangeant@gmail.com>
2019-05-30 09:26:15 -0700

    hello world

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { redner } from 'react-dom';
+import { render } from 'react-dom';
 import { Provider }  from 'react-redux';
-import store from './store';
 import App from './components/App';
 
-redner(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+render(
+  <App />,
   document.getElementById('root')
 );
